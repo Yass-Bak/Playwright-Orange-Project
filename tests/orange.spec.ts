d7f5c30d67f5d4c392ed2f2b0a093741d797a8c1
Yassine <bakloutiyassine1999@gmail.com>
2025-10-14 02:01:00 +0100

    Login Module

--- tests/orange.spec.ts
+++ tests/orange.spec.ts
@@ -1,13 +1,18 @@
 import { test, expect } from '@playwright/test';
 
-test('Login TC', async ({ page }) => {
+test('Login Success', async ({ page }) => {
     await page.goto('https://opensource-demo.orangehrmlive.com');
 
-    const username = page.getByRole('textbox', { name: 'Username' });
-    await username.fill('Admin');
-    const Password = page.getByPlaceholder('Password');
-    await Password.fill('admin123');
-    const submit = page.getByRole('button', { name: 'Login' });
-    submit.click();
+    await page.getByRole('textbox', { name: '用户名' }).fill('Admin');
+    await page.getByRole('textbox', { name: '密码' }).fill('admin123');
+    await page.getByRole('button', { name: '登录' }).click();
     await expect(page).toHaveURL('https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index');
+});
+test('Login Unsuccess', async ({ page }) => {
+    await page.goto('https://opensource-demo.orangehrmlive.com');
+
+    await page.getByRole('textbox', { name: '用户名' }).fill('admin123');
+    await page.getByRole('textbox', { name: '密码' }).fill('Admin');
+    await page.getByRole('button', { name: '登录' }).click();
+    expect(page.getByText('Invalid credentials')).toBeTruthy();
 });
